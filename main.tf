547d01206997540a2b97d8b2f529eb178fdbec18
siva4488 <93492753+siva4488@users.noreply.github.com>
2021-11-09 16:35:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ data "aws_network_interface" "default" {
 }
 
 data "aws_security_group" "default" {
-  name = managed.aws_security_group.default.instance.*.name
+  name = aws_security_group.default.instance.[0].name
 }
 
 data "aws_ebs_volume" "default" {
